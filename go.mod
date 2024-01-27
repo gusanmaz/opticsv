80fd04f70b4fc143aec3176c86f2721afaf4c6fd
Guvenc Usanmaz <gusanmaz@gmail.com>
2024-01-27 16:45:45 +0300

    Implement varying point, multiple correct answer mechanisms

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gusanmaz/opticsv
+
+go 1.22rc1
+
+require golang.org/x/text v0.14.0
--- main.go
+++ main.go
@@ -4,128 +4,226 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"golang.org/x/text/encoding/charmap"
+	"math"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-var multipleAnswerMap = map[byte]string{
+type AnswerKey struct {
+	Answer string
+	Points float64
+}
+
+var multipleAnswerMap = map[rune]string{
 	'F': "AB", 'G': "AC", 'H': "AD", 'I': "AE", 'J': "BC",
 	'K': "BD", 'L': "BE", 'M': "CD", 'N': "CE", 'O': "DE", 'X': "ABCDE",
 }
 
-func compare(teacher, student string) int {
-	correctCount := 0
-	for question, answer := range teacher {
-		studentAnswer := student[question]
-		if studentAnswer == ' ' {
-			continue
+func main() {
+	filename := flag.String("filename", "", "Base filename for the .dat and .key files (without extension)")
+	totalPoints := flag.Float64("totalPoints", 100, "Total points for the exam")
+	flag.Parse()
+
+	answerKeys, err := readAnswerKeys(*filename+".key", *totalPoints)
+	if err != nil {
+		fmt.Printf("Error reading answer keys: %v\n", err)
+		return
+	}
+
+	processedStudents, averageScore, err := processExam(*filename+".dat", *filename+".csv",
+		*filename+"_detailed.csv", answerKeys)
+	if err != nil {
+		fmt.Printf("Error processing exam: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Processed %d students. Average score: %.2f\n", processedStudents, averageScore)
+}
+
+func readAnswerKeys(keyFilePath string, totalPoints float64) (map[rune][]AnswerKey, error) {
+	file, err := os.Open(keyFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	answerKeys := make(map[rune][]AnswerKey)
+	scanner := bufio.NewScanner(file)
+	sheetTypes := "ABCDE"
+	answerPattern := regexp.MustCompile(`([A-X])(:\d+)?`)
+
+	for i := 0; scanner.Scan() && i < len(sheetTypes); i++ {
+		line := scanner.Text()
+		matches := answerPattern.FindAllStringSubmatch(line, -1)
+
+		if len(matches) == 0 {
+			return nil, fmt.Errorf("no valid answers found in line for sheet %c", sheetTypes[i])
 		}
 
-		multipleAnswers, ok := multipleAnswerMap[byte(answer)]
-		if ok {
-			if strings.Contains(multipleAnswers, string(studentAnswer)) {
-				correctCount++
+		var keys []AnswerKey
+		var totalDefinedPoints float64
+
+		for _, match := range matches {
+			answer := match[1]
+			var points float64
+			if match[2] != "" {
+				var err error
+				pointsStr := strings.TrimPrefix(match[2], ":")
+				points, err = strconv.ParseFloat(pointsStr, 64)
+				if err != nil {
+					return nil, fmt.Errorf("invalid points format for answer %s in sheet %c", answer, rune(sheetTypes[i]))
+				}
+				totalDefinedPoints += points
+			} else {
+				// Points not specified, assign a placeholder value for now
+				points = -1
 			}
-		} else {
-			if byte(answer) == studentAnswer {
-				correctCount++
-			}
+			keys = append(keys, AnswerKey{Answer: answer, Points: points})
+		}
+
+		// Distribute remaining points among answers without specified points
+		distributeRemainingPoints(keys, totalPoints, totalDefinedPoints)
+
+		answerKeys[rune(sheetTypes[i])] = keys
+	}
+
+	return answerKeys, scanner.Err()
+}
+
+func distributeRemainingPoints(keys []AnswerKey, totalPoints, totalDefinedPoints float64) {
+	remainingPoints := totalPoints - totalDefinedPoints
+	var countWithoutPoints int
+	for _, key := range keys {
+		if key.Points == -1 {
+			countWithoutPoints++
 		}
 	}
-	return correctCount
+	for i := range keys {
+		if keys[i].Points == -1 {
+			keys[i].Points = remainingPoints / float64(countWithoutPoints)
+		}
+	}
 }
 
-func getPoint(answerKey string, studentAnswers string, ppq float64) int {
-	correctCnt := compare(answerKey, studentAnswers)
-	return int(float64(correctCnt) * ppq)
-}
-
-func processExam(datFilePath, keyFilePath string) {
+func processExam(datFilePath, csvFilePath, detailedCsvFilePath string, answerKeys map[rune][]AnswerKey) (int, float64, error) {
 	dataFile, err := os.Open(datFilePath)
 	if err != nil {
-		fmt.Printf("Error opening data file: %v\n", err)
-		return
+		return 0, 0, err
 	}
 	defer dataFile.Close()
 
-	csvFile, err := os.Create(strings.TrimSuffix(datFilePath, ".dat") + ".csv")
+	reader := bufio.NewReader(charmap.Windows1254.NewDecoder().Reader(dataFile))
+
+	csvFile, err := os.Create(csvFilePath)
 	if err != nil {
-		fmt.Printf("Error creating CSV file: %v\n", err)
-		return
+		return 0, 0, err
 	}
 	defer csvFile.Close()
 
-	keyFile, err := os.Open(keyFilePath)
+	detailedCsvFile, err := os.Create(detailedCsvFilePath)
 	if err != nil {
-		fmt.Printf("Error opening key file: %v\n", err)
-		return
+		return 0, 0, err
 	}
-	defer keyFile.Close()
+	defer detailedCsvFile.Close()
 
-	scanner := bufio.NewScanner(keyFile)
-	var answerKeys []string
-	for scanner.Scan() {
-		answerKeys = append(answerKeys, scanner.Text())
-	}
+	var totalScore float64
+	var processedStudents int
 
-	totalQuestions := len(answerKeys[0])
-	ppq := 100.0 / float64(totalQuestions)
+	questionNumbers := len(answerKeys['A'])
 
-	dataScanner := bufio.NewScanner(dataFile)
-	regExpStr := fmt.Sprintf(`([0-9]{1,%d})([A-D])((?:[A-E ]){%d})`, 10, totalQuestions)
-	matchExp := regexp.MustCompile(regExpStr)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
 
-	studentCnt := 0
-	gradeSum := 0
-
-	for dataScanner.Scan() {
-		line := dataScanner.Text()
-		matches := matchExp.FindStringSubmatch(line)
-		if matches == nil {
-			fmt.Println("Invalid record:", line)
+		studentID, studentName, sheetType, answers, ok := parseLine(string(line), questionNumbers)
+		if !ok {
 			continue
 		}
 
-		studentNo, sheetType, studentAnswers := matches[1], matches[2], matches[3]
-		if len(studentNo) < 10 {
-			fmt.Printf("Incomplete student number: %s. Record will not be included in CSV.\n", studentNo)
+		score := calculateScore(answers, answerKeys[sheetType])
+		roundedScore := int(math.Round(score))
+
+		_, err = fmt.Fprintf(csvFile, "%s,%d\n", studentID, roundedScore)
+		if err != nil {
+			return processedStudents, totalScore / float64(processedStudents), err
 		}
 
-		sheetIndex := strings.Index("ABCD", sheetType)
-		if sheetIndex == -1 || sheetIndex >= len(answerKeys) {
-			fmt.Println("Invalid sheet type for student:", studentNo)
-			continue
+		_, err = fmt.Fprintf(detailedCsvFile, "%s,%s,%d\n", studentName, studentID, roundedScore)
+		if err != nil {
+			return processedStudents, totalScore / float64(processedStudents), err
 		}
 
-		point := getPoint(answerKeys[sheetIndex], studentAnswers, ppq)
-		if len(studentNo) == 10 {
-			fmt.Fprintf(csvFile, "%s;%d\n", studentNo, point)
-		} else {
-			fmt.Printf("Error for student with incomplete number %s: Calculated points %d\n", studentNo, point)
-		}
-
-		studentCnt++
-		gradeSum += point
+		processedStudents++
+		totalScore += score
 	}
 
-	avg := float64(gradeSum) / float64(studentCnt)
-	fmt.Printf("Processed %d students. Average score: %.2f\n", studentCnt, avg)
+	return processedStudents, totalScore / float64(processedStudents), nil
 }
 
-func main() {
-	filename := flag.String("filename", "", "Base filename for the .dat and .key files (without extension)")
-	flag.Parse()
+func parseLine(line string, questionNumber int) (studentID string, studentName string, sheetType rune, answers string, ok bool) {
+	runes := []rune(line)
 
-	if *filename == "" {
-		fmt.Println("Filename is required. Use -filename flag to specify it.")
-		os.Exit(1)
+	// Ensure the runes slice is long enough before slicing
+	if len(runes) < 33 {
+		fmt.Printf("Line too short: %s\n", line)
+		return "", "", ' ', "", false
 	}
 
-	datFilePath := *filename + ".dat"
-	keyFilePath := *filename + ".key"
+	// Extract studentName and studentID using rune slicing
+	studentName = string(runes[:20])
+	studentIDRaw := string(runes[23:33])
+	studentID = strings.TrimSpace(studentIDRaw)
 
-	processExam(datFilePath, keyFilePath)
+	// Ensure there's enough length for at least one character of answers
+	if len(runes) <= 33 {
+		fmt.Printf("Line too short to include answers: %s\n", line)
+		return "", "", ' ', "", false
+	}
+
+	// Extract sheetType and answers
+	sheetTypeChar := runes[33]
+	if sheetTypeChar >= 'A' && sheetTypeChar <= 'E' {
+		sheetType = rune(sheetTypeChar)
+	} else {
+		sheetType = 'A'
+	}
+
+	if len(runes) > 34 {
+		answers = string(runes[34:(34 + questionNumber)])
+	} else {
+		answers = ""
+	}
+
+	return studentID, studentName, sheetType, answers, true
 }
 
-//![Sekonic OMR Software SS](/images/sekonic_ss.png)
+func calculateScore(answers string, keys []AnswerKey) float64 {
+	var score float64
+	for i, answer := range answers {
+		if i >= len(keys) {
+			break
+		}
+		correctAnswer := keys[i].Answer
+
+		// Convert the correctAnswer to a rune if it's a single character
+		if len(correctAnswer) == 1 {
+			correctRune := rune(correctAnswer[0])
+
+			// Check if the answer is one of the multiple correct answers
+			if multipleAnswers, ok := multipleAnswerMap[correctRune]; ok {
+				if strings.ContainsRune(multipleAnswers, rune(answer)) {
+					score += keys[i].Points
+				}
+			} else if string(answer) == correctAnswer {
+				// Check if the answer matches the key directly
+				score += keys[i].Points
+			}
+		}
+	}
+	return score
+}
